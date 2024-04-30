d2377269329a1c1950f8c5131026f141194ba104
Dan Vanderkam <danvdk@gmail.com>
2024-04-30 15:37:07 -0400

    write a custom test

--- hw5/part1/tests/prob1/custom/cart_invalid_example.rs
+++ hw5/part1/tests/prob1/custom/cart_invalid_example.rs
@@ -2,14 +2,14 @@ extern crate part1;
 #[allow(unused_imports)]
 use part1::prob1::cart::Cart;
 
-// Copy of the provided cart_invalid_method1 test
 fn main() {
     match Cart::login("id1".to_string(), "pw1".to_string()) {
         Err(()) =>
             assert!(false),
         Ok(empty) => {
-            // `checkout` method should be inaccessible in empty state.
-            empty.checkout();
+            let cart = empty.add_item(10).add_item(32).checkout();
+            // add_item should be inaccessible from Checkout state
+            cart.add_item(32);
         }
     }
 }
