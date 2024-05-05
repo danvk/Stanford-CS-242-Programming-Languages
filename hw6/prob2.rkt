a205ab491fadc4ec04f285f68c2b424a0a5a0b0f
Dan Vanderkam <danvdk@gmail.com>
2024-05-05 12:40:52 -0400

    Complete problem 2

--- hw6/prob2.rkt
+++ hw6/prob2.rkt
@@ -14,6 +14,28 @@
 ; Task: Implement `eval` using `throw` and `try_except`.
 ; Note: You can define any other helper functions.
 
+(define (eval_help e)
+  (if
+    (number? e)
+    e
+    (let* (
+      [op (first e)]
+      [a (eval_help (second e))]
+      [b (eval_help (third e))])
+      (cond
+        [(equal? op "+") (+ a b)]
+        [(equal? op "-") (- a b)]
+        [(equal? op "*") (* a b)]
+        [(equal? op "/") (if (equal? b 0) (throw "DivZero") (/ a b))]
+        [else (throw "OpError")]
+      )
+    )
+  )
+)
+
 (define (eval e)
-  (void)
+  (try_except
+    (lambda () (eval_help e))
+    (lambda (msg) msg)
+  )
 )
