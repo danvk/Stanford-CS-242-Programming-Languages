65112a59713e01e3bc4d2103a22068c0cbe2e36a
Dan Vanderkam <danvdk@gmail.com>
2024-05-24 16:25:59 -0400

    complete prob02

--- hw8/prob02.lean
+++ hw8/prob02.lean
@@ -9,52 +9,28 @@ import .src.lnat
 -- 2. E-Right: e1 val, e2 → e2' -> e1 ⊛ e2 → e1 ⊛ e2'
 -- 3. E-Op:                        n1 ⊛ n2 → n1 * n2
 
--- e1: (1*2) + (4/2)
--- e2: 4
-
---    (1*2) + (4/2)
--- ->   2   + (4/2) E-Left
--- ->   2   +   2   E-Right
--- ->       4       E-Op
-
-def e1left : Expr := Expr.Op Op.Mul (Expr.Num 1) (Expr.Num 2)
-def e1right : Expr := Expr.Num 3
+--    1 + ((2*3) + 1)
+-- -> 1 + (  6   + 1)
 
 def e1 : Expr :=
   Expr.Op Op.Add
-  -- (Expr.Num 2)
-  e1left
-  e1right
-  -- (Expr.Op Op.Div (Expr.Num 4) (Expr.Num 2))
-
-def e1b : Expr :=
-  Expr.Op Op.Add
-  (Expr.Num 2)
-  (Expr.Num 3)
+  (Expr.Num 1)
+  (Expr.Op Op.Add
+    (Expr.Op Op.Mul (Expr.Num 2) (Expr.Num 3))
+    (Expr.Num 1))
 
 def e2 : Expr :=
-  Expr.Num 5
+  Expr.Op Op.Add
+  (Expr.Num 1)
+  (Expr.Op Op.Add
+    (Expr.Num 6)
+    (Expr.Num 1))
 
-lemma eval_e1be2 :
-  e1b ↦ e2 :=
-  eval.EOp Op.Add 2 3
-
--- e ↦ e' := eval e e'
-lemma eval_e1e1b :
-  e1 ↦ e1b :=
-  eval.ELeft Op.Add e1left (Expr.Num 2) e1right (eval.EOp Op.Mul 1 2)
-
-lemma eval_trans {a b c : Expr}
-  (e1: a↦b) (e2: b↦c) : a ↦ c :=
-  sorry
-
-theorem eval_full :
-  e1 ↦ e2 :=
+lemma eval_example :
+   e1 ↦ e2 :=
 begin
-  apply eval.ELeft
-
+  apply eval.ERight,
+  apply eval.ELeft,
+  apply eval.EOp,
+  apply val.VNum,
 end
-  -- eval_trans eval_e1e1b eval_e1be2
-  -- eval.ELeft Op.Add e1left (Expr.Num 2) e1right (eval.EOp Op.Mul 1 2)
-  -- eval (Expr.Op Op.Add e1left e1right) (Expr.Num 5)
-
