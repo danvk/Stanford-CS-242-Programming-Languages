0b3ab169eb196fa5bdbb75756120ddc28b4304ef
Dan Vanderkam <danvdk@gmail.com>
2024-05-24 17:41:57 -0400

    solve prob03, thanks Rylan!

--- hw8/prob03.lean
+++ hw8/prob03.lean
@@ -21,28 +21,43 @@ begin
     left,
     apply val.VNum,
   },
-  case Op: op e1 e2 v1 v2 {
-    cases v1,
+  case Op: op e1 e2 ihe1 ihe2 {
+    right,
+    cases ihe1 with e1val e1expr,
     {
-      cases v2,
+      cases e1val with n1,
+      cases ihe2 with e2val e2expr,
       {
-        left,
-        apply (exists.elim (inversion e1 v1)),
-        intros n1 hn1,
-        apply (exists.elim (inversion e2 v2)),
-        intros n2 hn2,
-        simp*,
-        -- maybe this isn't true?
+        -- e1 and e2 are both values
+        cases e2val with n2,
+        -- Goal: Exists (eval (Expr.Op op (Expr.Num n1) (Expr.Num n2)))
+        existsi (Expr.Num (apply_op op n1 n2)),
+        apply eval.EOp,
       },
       {
-        right,
-        apply (exists.elim v2),
-        intros e2' he,
-        existsi e2',
-        -- Goal: (Expr.Op op e1 e2)↦e'
-        -- I don't think this is true?
+        -- e1 is a value, e2 is an expression
+        cases e2expr with e2p,  -- uses "cases" to get the witness
+        existsi (Expr.Op op (Expr.Num n1) e2p),
+        apply eval.ERight,
+        repeat { assumption },
+      }
+    },
+    {
+      cases e1expr with e1p,
+      cases ihe2 with e2val e2expr,
+      {
+        -- e1 is an expression, e2 is a value
+        cases e2val with n2,
+        existsi (Expr.Op op e1p (Expr.Num n2)),
+        apply eval.ELeft,
+        assumption,
+      },
+      {
+        -- both are expressions; we'll apply ELeft
+        existsi (Expr.Op op e1p e2),
+        apply eval.ELeft,
+        assumption,
       }
     }
   }
 end
-
