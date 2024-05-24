81acb64c8cef1b4abc9878a509f99282b386b0b6
Dan Vanderkam <danvdk@gmail.com>
2024-05-24 17:13:42 -0400

    feeling kinda stuck on prob03, too!

--- hw8/prob03.lean
+++ hw8/prob03.lean
@@ -10,9 +10,39 @@ import .prob01
 --       left, right, existsi, cases ... with ...,
 --       induction, case ... : ... { ... }, simp *.
 
+--lemma inversion : ∀ e : Expr, (val e) → (∃ n : ℕ, e = Expr.Num n)
+
 theorem progress :
   ∀ e : Expr, (val e) ∨ (∃ e', e ↦ e') :=
-begin 
-  -- FILL IN HERE.
-  sorry
-end 
+begin
+  intro e,
+  induction e,
+  case Num: n {
+    left,
+    apply val.VNum,
+  },
+  case Op: op e1 e2 v1 v2 {
+    cases v1,
+    {
+      cases v2,
+      {
+        left,
+        apply (exists.elim (inversion e1 v1)),
+        intros n1 hn1,
+        apply (exists.elim (inversion e2 v2)),
+        intros n2 hn2,
+        simp*,
+        -- maybe this isn't true?
+      },
+      {
+        right,
+        apply (exists.elim v2),
+        intros e2' he,
+        existsi e2',
+        -- Goal: (Expr.Op op e1 e2)↦e'
+        -- I don't think this is true?
+      }
+    }
+  }
+end
+
