2b652c3bad7a32205b23e1ba197ba26e8449d096
Dan Vanderkam <danvdk@gmail.com>
2024-05-25 11:29:41 -0400

    complete prob04

--- hw8/prob04.lean
+++ hw8/prob04.lean
@@ -13,9 +13,23 @@ begin
   induction h1,
   -- why can't I use case CRefl here? CRefl
   case evals.CRefl : e {
-
+    -- Here we have e1 = e1' = e; this is an instance of CRefl
+    apply evals.CRefl,
   },
-  case evals.CStep : e emid e' {
-
+  case evals.CStep : e emid e' hsmall hbig hmide' {
+    -- e ↦ emid and emid ↦* e'
+    -- Goal: (Expr.Op op e e2) ↦* (Expr.Op op e' e2)
+    -- hsmall: e ↦ emid
+    -- We know that (Expr.Op op e e2) ↦ (Expr.Op op emid e2)
+    --   via eval.CLeft
+    -- hmide': (Expr.Op op emid e2) ↦* (Expr.Op op e' e2)
+    -- I want to apply evals.CStep with:
+    -- e = (Expr.Op op e e2)
+    -- e' = (Expr.Op op emid e2)
+    -- e'' = (Expr.Op op e' e2)
+    apply evals.CStep (Expr.Op op e e2) (Expr.Op op emid e2) (Expr.Op op e' e2),
+    apply eval.ELeft,
+    exact hsmall,
+    exact hmide',
   }
 end
