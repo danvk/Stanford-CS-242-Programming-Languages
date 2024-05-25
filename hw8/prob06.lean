b12421ad30497d3e53b9259fdcec6340d7d312eb
Dan Vanderkam <danvdk@gmail.com>
2024-05-25 13:02:32 -0400

    complete prob06!

--- hw8/prob06.lean
+++ hw8/prob06.lean
@@ -28,9 +28,25 @@ begin
     split,
     apply val.VNum,
     -- Goal: Expr.Op op e1 e2↦*Expr.Num (apply_op op n1 n2)
-    transitivity,
-    show Expr, from Expr.Op op (Expr.Num n1) e2,
-
-
+    -- Plan:
+    -- 1. via transitive_left, we have:
+    --    Expr.Op op e1 e2 ↦* Expr.Op op (Expr.Num n1) e2
+    -- 2. via transitive_right, we have:
+    --    Expr.Op op (Expr.Num n1) e2 ↦* Expr.Op op (Expr.Num n1) (Expr.Num n2)
+    -- 3. via eval.EOp
+    --    Expr.Op op (Expr.Num n1) (Expr.Num n2) ↦ Expr.Num (apply_op op n1 n2)
+    -- put this all together via transitivity
+    transitivity Expr.Op op (Expr.Num n1) (Expr.Num n2),
+    transitivity Expr.Op op (Expr.Num n1) e2,
+    apply transitive_left,
+    assumption,
+    apply transitive_right,
+    assumption,
+    assumption,
+    -- Expr.Op op (Expr.Num n1) (Expr.Num n2)↦*Expr.Num (apply_op op n1 n2)
+    apply evals.CStep,
+    show Expr, from Expr.Num (apply_op op n1 n2),
+    apply eval.EOp,
+    apply evals.CRefl,
   },
 end
