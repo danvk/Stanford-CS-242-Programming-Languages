b15a653d249cc8c24d2610f27a31fd7a1cf124cd
Dan Vanderkam <danvdk@gmail.com>
2024-05-25 11:44:44 -0400

    complete prob05

--- hw8/prob05.lean
+++ hw8/prob05.lean
@@ -5,6 +5,18 @@ lemma transitive_right (op : Op) (e1 e2 e2' : Expr) :
   (val e1) → (e2 ↦* e2')
   → (Expr.Op op e1 e2 ↦* Expr.Op op e1 e2') :=
 begin
-  -- FILL IN HERE.
-  sorry
+  intros hv1 h2big,
+  induction h2big,
+  case evals.CRefl : e {
+    apply evals.CRefl,
+  },
+  case evals.CStep : e2 emid e2' hsmall hbig hmide' {
+    -- goal: Expr.Op op e1 e2↦*Expr.Op op e1 e2'
+    --   e = Expr.Op op e1 e2
+    --  e' = Expr.Op op e1 emid
+    -- e'' = Expr.Op op e1 e2'
+    apply evals.CStep (Expr.Op op e1 e2) (Expr.Op op e1 emid) (Expr.Op op e1 e2'),
+    apply eval.ERight,
+    repeat { assumption },
+  }
 end
