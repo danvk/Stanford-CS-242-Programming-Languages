caba8cd7d9da46f40200611b9a593134871e085c
Dan Vanderkam <danvdk@gmail.com>
2024-05-25 11:05:18 -0400

    confusing error

--- hw8/prob04.lean
+++ hw8/prob04.lean
@@ -1,10 +1,21 @@
 -- IMPORTANT: See `src/lnat.lean` for the formalization of Lnat.
 import .src.lnat
 
+-- If e1 ↦∗ e1', then (e1 ⊛ e2) ↦∗ (e1' ⊛ e2)
+-- - CRefl: If e1 == e1', then this is trivial
+-- - CStep:
+
 lemma transitive_left (op : Op) (e1 e1' e2 : Expr) :
   (e1 ↦* e1')
   → (Expr.Op op e1 e2 ↦* Expr.Op op e1' e2) :=
 begin
-  -- FILL IN HERE.
-  sorry
+  intros h1,
+  induction h1,
+  -- why can't I use case CRefl here? CRefl
+  case evals.CRefl : e {
+
+  },
+  case evals.CStep : e emid e' {
+
+  }
 end
