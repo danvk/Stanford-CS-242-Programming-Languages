3ecf2ad3af004cdf70d6fdeaa44c3d5ed2466990
Dan Vanderkam <danvdk@gmail.com>
2024-05-27 19:04:49 -0400

    complete prob11, the final problem!!!

--- hw8/prob11.lean
+++ hw8/prob11.lean
@@ -2,20 +2,37 @@
 import .src.lnatplus
 
 def e1 : Expr :=
-  -- FILL IN HERE.
-  sorry
+  Expr.Let (Expr.Num 2) (Expr.Var 1)
 
 def e2 : Expr :=
-  -- FILL IN HERE.
-  sorry
+  Expr.Var 1
 
 def i : ℕ :=
-  -- FILL IN HERE.
-  sorry
+  1
 
 lemma type_preserve_gen_counterexample :
   (typnat i e1) ∧ (e1 ↦ e2) ∧ ¬(typnat i e2) :=
 begin
-  -- FILL IN HERE.
-  sorry
+  split,
+  {
+    apply typnat.TLet,
+    apply typnat.TNum,
+    apply typnat.TVar,
+    unfold i,  -- had to ask ChatGPT for this!
+    exact nat.lt_succ_self 1,
+  },
+  split,
+  {
+    apply eval.DLet,
+    apply subst.SVarNeq,
+    exact nat.zero_ne_one,
+  },
+  {
+    assume h,
+    cases h,
+    exact nat.lt_irrefl 1 h_ᾰ
+  }
 end
+
+-- let 2 in 1^  -- typnat 1
+-- 1^           -- typnat 2!
